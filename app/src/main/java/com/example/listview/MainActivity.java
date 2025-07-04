45269bb737a494d60b020b1b1b702cb32d85f8f2
Syed <bughunter373@gmail.com>
2025-07-04 18:38:03 +0600

    # AsiaCountryListView
    An Android app that shows a list of Asian countries in a ListView and displays a Toast message when a country is clicked.
    
    ## 📱 Features
    - Simple ListView of Asian countries
    - Shows a Toast with country name on click
    - Clean and beginner-friendly project structure
    
    ## 🛠 Built With
    - Java
    - Android Studio
    - ListView
    - Toast
    
    ## 🚀 Why this project?
    This project was created as a practice to learn how to use ListView, handle item click events and show Toast messages in Android.
    
    ## ✨ Author
    - **Syed Shihab** – [GitHub Profile](https://github.com/shibusec)
    
    ---
    
    ⭐ If you like this project, please give it a star!

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.listview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView ;
+    String[] countries ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.List);
+        countries = getResources().getStringArray(R.array.country_capitals);
+        ArrayAdapter add = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,countries);
+        listView.setAdapter(add);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(MainActivity.this, "You Clicked " + countries [position], Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
